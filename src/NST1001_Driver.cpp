35722d9e743acf14178a6f747e3c01ab4ac5ab24
Joey Strandnes <joey.strandnes@hotmail.com>
2021-01-04 00:31:07 +0100

    Speed optimization and more reliable
    Made code rely on pin state changes instead of "hoping" that the total time would be under a specific time.
    Function execution time reduced by ~15%.
    I think, I’m tired...

--- src/NST1001_Driver.cpp
+++ src/NST1001_Driver.cpp
@@ -5,46 +5,49 @@
 #include "NST1001_Driver.h"
 
 
+//Will declare varibles in constructor...
 NST1001::NST1001(){
-
-  
-
-  
 }
- 
+
 const void NST1001::init(int const Enable_Pin, int const Temp_Unit){
 /*  Setup timer-counter 1/16-bit timer  
  *  Maximum number of pulses = 3201 => 150 degrees celsius
 */
-  
   cli();
   EN_Pin = Enable_Pin;
   Unit   = Temp_Unit;
 
-  
   pinMode(EN_Pin, OUTPUT);
   
-//Timer 1 Normal mode with falling edge as clock source and noise suppression
+//Timer 1 Normal mode with falling edge as clock source.
   TCCR1A = (0<<COM1A1)|(0<<COM1A0)|(0<<COM1B1)|(0<<COM1B0)|(0<<WGM11)|(0<<WGM10);
-  TCCR1B = (1<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(0<<WGM12)|(1<<CS12)|(1<<CS11)|(0<<CS10); 
+  TCCR1B = (0<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(0<<WGM12)|(1<<CS12)|(1<<CS11)|(0<<CS10); 
   TIMSK1 = (0<<ICIE1)|(0<<OCIE1B)|(0<<OCIE1A)|(0<<TOIE1);
   TCNT1 = 0;
 
   sei();
 }
 
-
 const float NST1001::getTemp(){
-
+  
   float Temp = 0;
 
   digitalWrite(EN_Pin, HIGH);
-
+  
   TCNT1 = 0;                            // Reset counter value
-  _delay_ms(50);                        // Conversion + transmission delay (Longest possible time)
-  Temp = ((TCNT1*0.0625) - 50.0625);    // Calculate temperature from sampled pulses
+  
+  while(TCNT1 == 0);                    // First ~24ms of ADC conversion time.
+  
+  while(1){                             // Rest of the unknown transmission time.
+    Temp = TCNT1;
+    _delay_us(9);  
+    if(Temp == TCNT1){
+      break;
+    }
+  }
 
   digitalWrite(EN_Pin, LOW);
+  Temp = ((TCNT1*0.0625) - 50.0625);    // Calculate temperature from sampled pulses
   
 //Temp compensation, no compensation is needed in the interval 30 ≤ T ≤ 100 
   if(Temp < 30){
@@ -64,6 +67,7 @@ const float NST1001::getTemp(){
   else if(Unit == 2){
     Temp += 273.15;
   }
+
   return Temp;
 }
 
--- src/NST1001_Driver.h
+++ src/NST1001_Driver.h
@@ -2,9 +2,7 @@
 #ifndef NST1001_DRIVER_H
 #define NST1001_DRIVER_H
 
-#include <Arduino.h>
-#include <util/delay.h>
-
+#include <Arduino.h> //Arduino already includes delay and io
 
 class NST1001{
 
@@ -12,10 +10,10 @@ class NST1001{
     NST1001();
     const void  init(int const Enable_Pin, int const Temp_Unit = 0);
     const float getTemp();
-
+    
   private:
-    int EN_Pin{};
-    int Unit{};
+    int   EN_Pin{};
+    int   Unit{};
     //int MultiModePins[];
     //Multi mode...
 };

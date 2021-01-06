e9e0e53e24759a60413c073afb6e303270a7e402
Joey Strandnes <joey.strandnes@hotmail.com>
2021-01-06 20:57:57 +0100

    Added an example to read the temperature.

---
+++ examples/Read_Temperature/Read_Temperature.ino
@@ -0,0 +1,33 @@
+/* NST1001 read temperature example!
+ * The sensor data pin must be connected to pin "T1". Since it uses the 16-bit hardware counter. 
+ * T1:UNO   = 5
+ * T1:Nano  = 8
+ * T1:Micro = 26
+ * 
+ * getTemp returns the temperature as a float value.
+ * getTemp will return a temperature of 1000 if an error with the sensor occurs.
+ */
+#include <NST1001.h>
+
+
+float Temperature{0};
+const int  En_Pin{8}; // Any digital pin can be used as enable pin! 
+const char Unit{'C'}; // The available units are Celsius(C), Fahrenheit(F) and Kelvin(K).
+
+NST1001 nst1001(En_Pin, Unit);
+
+
+void setup(){
+
+  Serial.begin(115200);
+  nst1001.init();
+  
+}
+
+void loop() {
+
+  Temperature = nst1001.getTemp();
+  
+  Serial.println("The temperature is: " + String(Temperature) + Unit);
+  delay(1000);
+}

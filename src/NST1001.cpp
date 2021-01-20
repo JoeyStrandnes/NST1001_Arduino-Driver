7bd703b202a3811668e242945a5c0842f3cb4eb6
Joey Strandnes <joey.strandnes@hotmail.com>
2021-01-20 19:13:23 +0100

    Seems to work fine, still strange problem with only seing ~3 pulses sometimes.

--- src/NST1001.cpp
+++ src/NST1001.cpp
@@ -1,5 +1,7 @@
-/*ToDo: 
- * Add Pulldown mode and "Multicast" mode
+/* NST1001 driver implementation for the Arduino embedded platform.
+ * Supported architectures: AVR
+ * Date: 20/01-2021
+ * Joey Strandnes
  */
 #include "NST1001.h"
 
@@ -41,32 +43,31 @@ const float NST1001::getTemp(int const Index){ // Index is only used in multicas
   
   TCCR1B |= (0<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(0<<WGM12)|(1<<CS12)|(1<<CS11)|(0<<CS10); // Enable counter.
   
-  if(MultiCast == true){                      //Start sensor
-    _delay_ms(10);
+  if(MultiCast == true){                                    //Start sensor
+    _delay_ms(5);                                           // Requires some initial delay to settle. Need to look into this.
     pinMode(MultiCastPins[Index], OUTPUT);
   }
   else{
     digitalWrite(EN_Pin, HIGH);
  }
 
-  TCNT1 = 0;                              // Reset counter value.
+  TCNT1 = 0;                                                // Reset counter value.
   
-  //while(TCNT1 == 0);                    // First ~24ms of ADC conversion time. Will get stuck if pin never changes. Can use millis or WTD but dont want to >:(
-  for(int i = 0; i < 50; i++){            // Same as the row above but with a timeout. ~50ms timeout, fault is set if reached.    
+  for(int i = 0; i < 50; i++){                              // First ~24ms of ADC conversion time. ~50ms timeout, fault is set if reached. Async function.    
     if (TCNT1 > 0){
       break;
     }
     else if(TCNT1 == 0 && i == 49){
       Fault = true;
-    }    
+    }
     _delay_ms(1);
   }
   
-  while(1){                             // Rest of the unknown transmission time. A 11 micro-second delay is longest possible pulse time.
+  while(1){                                                 // Rest of the unknown transmission time. A 11 micro-second delay is longest possible pulse time.
     Temp = TCNT1;
     _delay_us(11);  
-    if(Temp == TCNT1 || TCNT1 == 3201 || Fault == true){ // Check if transmission is finished by checking if the pulse train has stopped, reached max or skip if fault is detected.   
-      TCCR1B = 0;                       // Disable counter.
+    if(Temp == TCNT1 || TCNT1 >= 3201 || Fault == true){    // Check if transmission is finished by checking if the pulse train has stopped, reached max or skip if fault is detected.   
+      TCCR1B = 0;                                           // Disable counter.
       if(MultiCast == true){
         pinMode(MultiCastPins[Index], INPUT);
       }
@@ -77,7 +78,7 @@ const float NST1001::getTemp(int const Index){ // Index is only used in multicas
     }
   }
 
-  if(TCNT1 < 1 || TCNT1 > 3201 || Fault == true){// Sanity check, returns "huge" value
+  if(TCNT1 < 1 || TCNT1 > 3201 || Fault == true){           // Sanity check, returns "huge" value
     Temp = 1000;
   }
   else{
